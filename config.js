958999fee9ef936c03c28ef5c7888b980c76cda5
MistrzCieniPL <malanowskifilip22@gmail.com>
2026-09-11 14:32:38 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
+const CONFIG = {
+    name: "ShadowTale",
+    domain: "ShadowTale.pl",
 
+    supabaseUrl: "https://vbxyshxmnsfnlwuihzse.supabase.co",
+    supabaseAnonKey: "sb_publishable_XszGm5td2PP0Tso0ZEX0-g_dfVQEILw"
+};
